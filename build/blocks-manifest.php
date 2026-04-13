6eaef5c9884840a83d6ad8b6de8eb9f851767ce6
ajmarvingomez <ajmarvingomez@gmail.com>
2026-04-13 11:54:59 -0400

    build: Build stat block

--- build/blocks-manifest.php
+++ build/blocks-manifest.php
@@ -48,5 +48,57 @@ return array(
 		'style' => 'file:./style-index.css',
 		'render' => 'file:./render.php',
 		'viewScript' => 'file:./view.js'
+	),
+	'stats' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'game-2026/game-2026-stats',
+		'version' => '0.1.0',
+		'title' => 'Game 2026 Stats',
+		'category' => 'widgets',
+		'icon' => 'smiley',
+		'description' => 'Stats',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'usesContext' => array(
+			'postType',
+			'postId'
+		),
+		'attributes' => array(
+			'strength' => array(
+				'type' => 'number',
+				'default' => 10
+			),
+			'dexterity' => array(
+				'type' => 'number',
+				'default' => 10
+			),
+			'constitution' => array(
+				'type' => 'number',
+				'default' => 10
+			),
+			'intelligence' => array(
+				'type' => 'number',
+				'default' => 10
+			),
+			'wisdom' => array(
+				'type' => 'number',
+				'default' => 10
+			),
+			'charisma' => array(
+				'type' => 'number',
+				'default' => 10
+			)
+		),
+		'textdomain' => 'game-2026',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'render' => 'file:./render.php',
+		'viewScript' => 'file:./view.js'
 	)
 );
