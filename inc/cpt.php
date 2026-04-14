3fb5244727eea5b3555bac9679d379f391e4fd3d
ajmarvingomez <ajmarvingomez@gmail.com>
2026-04-14 11:16:11 -0400

    feat: Register Armor custom post type

--- inc/cpt.php
+++ inc/cpt.php
@@ -21,3 +21,21 @@ function game_2026_monster_custom_post_type()
 }
 
 add_action('init', 'game_2026_monster_custom_post_type');
+
+
+function game_2026_armor_custom_post_type()
+{
+    $labels = array(
+        'name' => 'Armors',
+        'singular_name' => 'Armor',
+        'menu_name' => 'Armors'
+    );
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'thumbnail', 'revisions', 'custom-fields'),
+    );
+    register_post_type('armor', $args);
+}
+add_action('init', 'game_2026_armor_custom_post_type');
